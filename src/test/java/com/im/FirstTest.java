9953bb506243958b10423577d2e234753d7f00d5
Imran Mir <imran.mir@pwc.com>
2020-02-26 00:12:26 +0530

    Added a canary test

--- src/test/java/com/im/FirstTest.java
+++ src/test/java/com/im/FirstTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FirstTest {
-
+    @Test
+    void canary(){
+        assertTrue(true);
+    }
 
 }
